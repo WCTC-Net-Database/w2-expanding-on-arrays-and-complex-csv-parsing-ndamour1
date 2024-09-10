5ff11ba8d0faf970e4cc53b34479597eb90db201
ndamour1 <143230031+ndamour1@users.noreply.github.com>
2024-09-09 21:28:04 -0400

    Update Character.cs
    Completely forgot about this last time.

--- Character.cs
+++ Character.cs
@@ -8,5 +8,33 @@ namespace W2_assignment_template
 {
     internal class Character
     {
+        public string name { get; set; }
+        public string characterClass { get; set; }
+        public int level { get; set; }
+        public int hitPoints { get; set; }
+        public string[] equipment { get; set; }
+    
+        public override string ToString()
+        {
+            if (name.Contains(", "))
+            {
+                int commaIndex = name.IndexOf(',');
+                string firstName = name.Substring(commaIndex + 2);
+                string lastName = name.Substring(0, commaIndex);
+                name = $"{firstName} {lastName}";
+            }
+            string characterText = $"{name} the {characterClass}\nLevel: {level}\nHP: {hitPoints}\nEquipment: ";
+            for (int i = 0; i < equipment.Length; i++) {
+                if (i != equipment.Length - 1)
+                {
+                    characterText += $"{equipment[i]}, ";
+                }
+                else
+                {
+                    characterText += $"{equipment[i]}\n";
+                }
+            }
+            return characterText;
+        }
     }
 }
